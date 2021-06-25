6b6d5728f3b3cfe7b09a2a66568bd518aa959bca
INdIEDeV016 <80198819+INdIEDeV016@users.noreply.github.com>
2021-06-25 18:17:34 +0530

    Paging (half done)

--- addons/tutorial/Scenes/InfoDialog.gd
+++ addons/tutorial/Scenes/InfoDialog.gd
@@ -3,18 +3,19 @@ extends Panel
 
 signal page_changed
 
-var page: int = 0
+var page: int = 1
 var total_pages: int
 var content: Array
 var drag_position
 
 onready var title: String
-onready var title_label = $Title
-onready var rtl = $RichTextLabel
-onready var page_label = $Footer/Page
-onready var previous_button = $Footer/HBoxContainer/Previous
-onready var home_button = $Footer/HBoxContainer/HomeButton
-onready var next_button = $Footer/HBoxContainer/Next
+onready var title_label = $VBoxContainer/Title
+onready var rtl = $VBoxContainer/RichTextLabel
+onready var note_label = $VBoxContainer/Footer/VBoxContainer/Note
+onready var page_label = $VBoxContainer/Footer/VBoxContainer/Page
+onready var previous_button = $VBoxContainer/Footer/VBoxContainer/HBoxContainer/Previous
+onready var home_button = $VBoxContainer/Footer/VBoxContainer/HBoxContainer/HomeButton
+onready var next_button = $VBoxContainer/Footer/VBoxContainer/HBoxContainer/Next
 
 
 func _ready() -> void:
@@ -30,16 +31,20 @@ func _ready() -> void:
 		title_label.text = "Error"
 		match err:
 			7:
-				rtl.text = str("ERR_FILE_NOT_FOUND: A file called %s.json was not found in the TutorialContents (res://addons/tutorial/TutorialContents/) Folder" % title)
+				rtl.bbcode_text = str("ERR_FILE_NOT_FOUND: A file called %s.json was not found in the TutorialContents (res://addons/tutorial/TutorialContents/) Folder" % title)
 	f.close()
+	total_pages = content.size()
 #	print(title)
-	rtl.text = content[0]["content"]
-	if content[0].has("position"):
-		rect_global_position.x = content[0]["position"]["x"]
-		rect_global_position.y = content[0]["position"]["y"]
+	rtl.bbcode_text = content[0]["content"]
+	if content[page - 1].has("position"):
+		rect_global_position.x = content[page - 1]["position"]["x"]
+		rect_global_position.y = content[page - 1]["position"]["y"]
 	previous_button.hide()
-	page_label.text = "Page: %s / %s" % [page, content.size()]
+	page_label.text = "Page: %s / %s" % [page, total_pages]
 
+func _process(delta: float) -> void:
+	rect_size = $VBoxContainer.rect_size
+	
 
 func _on_InfoDialog_popup_hide() -> void:
 	call_deferred("popup")
@@ -52,44 +57,41 @@ func _on_HomeButton_pressed() -> void:
 
 func _on_Next_pressed() -> void:
 	emit_signal("page_changed")
-	if page < content.size() - 1: page += 1
-	else: page = content.size()
-	
-	rtl.text = content[page]["content"]
-	if content[page].has("title"):
-		title_label.text = title + ": " + content[page]["title"]
-	else:
-		title_label.text = title
-	if content[page].has("position"):
-		rect_global_position.x = content[page]["position"]["x"]
-		rect_global_position.y = content[page]["position"]["y"]
+	page += 1
+	if page > content.size() - 1: page = content.size() - 1
 
 
 func _on_Previous_pressed() -> void:
 	emit_signal("page_changed")
-	if page > 0: page -= 1
-	else: page = 0
-	
-	rtl.text = content[page]["content"]
-	if content[page].has("title"):
-		title_label.text = title + ": " + content[page]["title"]
-	else:
-		title_label.text = title
-	if content[page].has("position"):
-		rect_global_position.x = content[page]["position"]["x"]
-		rect_global_position.y = content[page]["position"]["y"]
+	page -= 1
+	if page < 0: page = 0
 
 func handle_pages():
-	page_label.text = "Page: %s / %s" % [page, content.size()]
+	rtl.bbcode_text = content[page - 1]["content"]
+	page_label.text = "Page: %s / %s" % [page + 1, content.size()]
 	if page == 0:
 		previous_button.hide()
 	else:
 		previous_button.show()
 	
+	if content[page].has("title"):
+		title_label.text = title + ": " + content[page]["title"]
+	
 	if page == content.size():
 		next_button.text = "Home"
 	else:
 		next_button.text = "Next >>"
+	
+	if content[page].has("note"):
+		note_label.show()
+		note_label.text = "Note: " + content[page]["note"]
+	else:
+		note_label.text = ""
+		note_label.hide()
+	
+	if content[page].has("position"):
+		rect_global_position.x = content[page]["position"]["x"]
+		rect_global_position.y = content[page]["position"]["y"]
 
 func _on_InfoDialog_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
